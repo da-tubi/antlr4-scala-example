328d1603cb7704429d336a2b294f2541a9b4df0a
Da <da@tubi.tv>
2020-09-04 16:50:50 +0800

    mill example.reformat

--- example/src/ArithmeticVisitorTest.scala
+++ example/src/ArithmeticVisitorTest.scala
@@ -26,9 +26,9 @@ object ArithmeticVisitorTest extends App {
     println("\nEvaluating expression: " + input)
 
     val charStream = new ANTLRInputStream(input)
-    val lexer      = new ArithmeticLexer(charStream)
-    val tokens     = new CommonTokenStream(lexer)
-    val parser     = new ArithmeticParser(tokens)
+    val lexer = new ArithmeticLexer(charStream)
+    val tokens = new CommonTokenStream(lexer)
+    val parser = new ArithmeticParser(tokens)
 
     val arithmeticVisitor = new ArithmeticVisitorApp
     val res = arithmeticVisitor.visit(parser.expr())
@@ -42,22 +42,30 @@ object ArithmeticVisitorTest extends App {
       println(s"Expression after tokenization = $exprText")
 
       val operands = ctx.NUMBER().toList.map(_.getText)
-      val operand1 = parseDouble(operands.lift(0).getOrElse("0.0")).getOrElse(0.0)
-      val operand2 = parseDouble(operands.lift(1).getOrElse("0.0")).getOrElse(0.0)
+      val operand1 =
+        parseDouble(operands.lift(0).getOrElse("0.0")).getOrElse(0.0)
+      val operand2 =
+        parseDouble(operands.lift(1).getOrElse("0.0")).getOrElse(0.0)
 
       val operation = visitOperation(ctx.operation())
 
       ExprResult(calculate(operand1, operand2, operation.name))
     }
 
-    override def visitOperation(ctx: ArithmeticParser.OperationContext): Operation = {
+    override def visitOperation(
+        ctx: ArithmeticParser.OperationContext
+    ): Operation = {
       val op = ctx.getText
       Operation(op)
     }
 
     def parseDouble(s: String): Option[Double] = Try(s.toDouble).toOption
 
-    def calculate(op1: Double, op2: Double, operation: String): Option[Double] = {
+    def calculate(
+        op1: Double,
+        op2: Double,
+        operation: String
+    ): Option[Double] = {
       operation match {
         case "+" => Some(op1 + op2)
         case "-" => Some(op1 - op2)
